c787c24d7e6b3cdd69559a70b20baf36348d229e
Victoria Tiller <vijotiller@gmail.com>
2021-05-20 14:40:54 -0400

    very basic layout

--- public/main.js
+++ public/main.js
@@ -1,7 +1,3 @@
-function main() {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
-}
+function main() {}
 
 document.addEventListener('DOMContentLoaded', main)
